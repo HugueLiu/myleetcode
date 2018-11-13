9cd058a0e6447a53d6e52b87cfb026a599b57301
BigMoWangying <wwying1996@njust.edu.cn>
2018-11-13 09:49:53 +0800

    003 use map

--- 003 Longest Substring Without Repeating Characters/wy.cpp
+++ 003 Longest Substring Without Repeating Characters/wy.cpp
@@ -1,5 +1,5 @@
 //too slow O(n2)
-
+/*
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
@@ -26,3 +26,42 @@ public:
         return max_num;
     }
 };
+*/
+
+#include <string>
+#include <map>
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+class Solution
+{
+ public:
+    Solution(){};
+    int lengthOfLongestSubstring(string s)
+    {
+        map<char, int> m;
+        int maxLen = 0;
+        int lastRepeatPos = -1;
+
+        for(int i = 0; i < s.size(); i++)
+        {
+            if(m.find(s[i]) != m.end() && lastRepeatPos < m[s[i]])
+            {
+                lastRepeatPos = m[s[i]];
+            }
+            if(i - lastRepeatPos > maxLen)
+            {
+                maxLen = i - lastRepeatPos;
+            }
+            m[s[i]] = i;
+        }
+        return maxLen;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
